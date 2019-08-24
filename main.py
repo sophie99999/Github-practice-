b69491979df66f1d7becceadaf2df0a044d4d253
Yuefei Wang <yuefeiwang@Yuefeis-MacBook-Pro.local>
2019-08-24 14:08:59 -0700

    new python file

---
+++ main.py
@@ -0,0 +1,19 @@
+# @TODO: Write a function that returns the arithmetic average for a list of numbers
+
+
+# Test your function with the following:
+# print(average([1, 5, 9]))
+# print(average(range(11)))
+
+def average (numbers)
+    length=len(numbers)
+    total=0.0
+    for number in numbers,
+    total+=number
+    return total/length
+
+print(average([1,5,9]))
+print(average(range(11)))
+
+
+
